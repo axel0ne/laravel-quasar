8ac6118baa91ebdb155467cc5ff4b419cda59d3f
axel0ne <aleksan94@yandex.ru>
2022-09-17 03:00:38 +0300

    Check install app

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
-cd quasarapp
-npm install
-quasar build
-cd ..
-composer install
-npm install
-cp -r ./web/. ./public
-php artisan migrate
-bash cache.sh
+FILE=.env
+if [ ! -f "$FILE" ]; then
+  echo 'App is not installed'
+else
+  cd quasarapp
+  npm install
+  quasar build
+  cd ..
+  composer install
+  npm install
+  cp -r ./web/. ./public
+  php artisan migrate
+  bash cache.sh
+fi
